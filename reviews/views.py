07100dddf1643b249c1f74ac9796e2930dc11b51
knch1014 <knch1014@gmail.com>
2021-11-23 10:34:28 +0900

    like기능 구현

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import json, boto3
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from .models import Review, ReviewImage, Like
+from users.models import User
+from products.models import  Option
+from core.utils import signin_decorator
+from django.conf  import settings
+
+class LikeView(View):
+    @signin_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user_id = request.user.id
+            review_id = data['review_id']
+        
+            like, created = Like.objects.get_or_create(
+                user_id = user_id,
+                review_id = review_id
+            )
+
+            if not created:
+                like.delete()
+
+            review = Review.objects.get(id=review_id)
+            results={
+                    'likes' : len(review.like_set.all()),
+                    'review_id' : review_id
+            }
+
+            return JsonResponse({'results' : results}, status=201)
+
+        except KeyError :
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
