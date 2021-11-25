a47eb5946a5558b0fc4167ea181afa1c85c132d8
knch1014 <knch1014@gmail.com>
2021-11-25 11:29:09 +0900

    좋아요 수정

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from reviews.views import ReviewView
+from reviews.views import ReviewView, LikeView
 
 urlpatterns=[
     path('/review' , ReviewView.as_view()),
+    path('/review/like', LikeView.as_view())
 ]
